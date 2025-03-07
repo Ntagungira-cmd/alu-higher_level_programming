d44b51898ba23e6116aead780f51e7c1acdeb906
Ntagungira-cmd <r.ntagungir@alustudent.com>
2025-03-07 07:08:22 -0800

    hidden_4.pyc

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-result = 0
-for arg in sys.argv[1:]:
-    result += int(arg)
-print(result)
+if __name__ == "__main__":
+    result = 0
+    for arg in sys.argv[1:]:
+        result += int(arg)
+    print(result)
